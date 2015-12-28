10542676fb84e7d9f6fa3304800f08c468c2da5e
Martin Siggel <martin.siggel@dlr.de>
2015-12-28 11:35:11 +0100

    Removed system libraries from OCE exports file on linux
    This is necessary to provide a relocatable package. Linux
    distributions install system libraries into different locations.
    Hard coded paths are therefore problematic. As the OCE libs are
    linked dynamically, there is no need to relink the system libs.
    
    Fixes issue #21

---
+++ conda-pkg/OCE/OCE-016/remove-system-libs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# @author: Martin Siggel <martin.siggel@dlr.de>
+#
+# This script fixes the cmake exports file by
+# removing explicit linking to system libraries
+
+import sys, re
+
+def remove_absolute_paths(line):
+    """
+    Removes libraries from the line that are found under /usr
+    """
+    return re.sub('/usr/[-_a-zA-Z0-9/]+.so[;]?', '', line)
+
+def fix_paths(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+        # just select lines containing string IMPORTED_LINK_INTERFACE_LIBRARIES
+        for i, line in enumerate(lines):
+            if "IMPORTED_LINK_INTERFACE_LIBRARIES" in line:
+                lines[i] = remove_absolute_paths(line)
+        
+    fout = open(filename,'w')
+    fout.write("".join(lines))
+    fout.close()
+
+if __name__ == "__main__":
+    assert(len(sys.argv) == 2)
+
+    filename = sys.argv[1]
+    fix_paths(filename)
+
+
